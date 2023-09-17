19a510c8aaf510afad649b2bed5dfb401a6e5942
Mallireddy Bhoomika <bhoomikamallireddy@gmail.com>
2023-09-17 02:44:11 -0700

    clealinesss project

--- int_dat.py
+++ int_dat.py
@@ -1,18 +1,20 @@
-import pymongo
+from pymongo import MongoClient
 from gridfs import GridFS
 
-# Replace with your MongoDB connection URI
-mongo_uri = "mongodb://my-mongodb-container:27017/"
+# Replace "<mongodb_host_ip>" with the actual IP address "10.32.44.84"
+connection_url = "mongodb://10.32.44.84:27017/"
 
-# Replace 'your_database' with the name of your database
-database_name = "your_database"
+# Create a MongoClient and connect to the remote MongoDB server
+client = MongoClient(connection_url)
+
+# Replace 'test' with the name of your database (which is "test" in this case)
+database_name = "test"
 
 # Replace 'image.jpg' with the path to the image file you want to store
-image_file_path = "image.jpg"
+image_file_path = "rail.jpg"
 
 try:
-    # Connect to MongoDB
-    client = pymongo.MongoClient(mongo_uri)
+    # Access the desired database
     db = client[database_name]
 
     # Initialize GridFS
@@ -20,7 +22,7 @@ try:
 
     # Open and store the image in GridFS
     with open(image_file_path, "rb") as image_file:
-        fs.put(image_file, filename="image.jpg")
+        fs.put(image_file, filename="rail.jpg")
 
     print("Image stored in MongoDB GridFS")
 
